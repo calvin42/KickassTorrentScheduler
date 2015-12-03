2825f485017c2721dce86f2fc5a3dbe2dbbdf242
Claudio Kerov <ckerov@gmail.com>
2015-12-03 11:22:21 +0100

    Aggiunta mini analisi

---
+++ katscheduler.py
@@ -0,0 +1,15 @@
+import kat 
+from crontab import CronTab
+
+
+#==========================#
+#	TODO
+#	Cercare disponibilita' torrent
+#	Scegliere il tipo di torrent (salvarsi internamente com'e' fatto il nome)
+#	Scegliere la data
+#	
+#	Divisione tra gestore dello scheduler e attivita' dello scheduler
+#	Salvare in un file i cronjob che faranno partire questo stesso script (o comunque uno solo)
+#	Quindi ci sara' una funzione che prende in input la data odierna e scarichera' cosa c'e' scritto nel file
+#	
+#==========================#
